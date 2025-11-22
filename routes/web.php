ee11ae365674834319c7d135d6b2b074e71010fd
CoderTX1 <coder1.travelx@gmail.com>
2025-11-22 11:29:08 +0300

    route

--- routes/web.php
+++ routes/web.php
@@ -74,37 +74,3 @@ Route::middleware(['auth', 'sa'])
             [SupportInboxController::class, 'stream']
         )->name('conversations.stream');
     });
-
-    ->prefix('superadmin')
-    ->group(function () {
-        Route::get('/conversations', [SupportInboxController::class, 'index'])
-            ->name('admin.conversations');
-
-        Route::get('/conversations/{business}', [SupportInboxController::class, 'users'])
-            ->name('admin.conversations.show');
-
-        Route::post(
-            '/conversations/{business}/user/{user}/reply',
-            [SupportInboxController::class, 'replyToUser']
-        )->name('admin.conversations.reply_user');
-
-        Route::post(
-            '/conversations/{business}/user/{user}/ack',
-            [SupportInboxController::class, 'ackUser']
-        )->name('admin.conversations.ack_user');
-
-        Route::get(
-            '/conversations/counters',
-            [SupportInboxController::class, 'counters']
-        )->name('admin.conversations.counters');
-
-        Route::get(
-            '/conversations/counters-map',
-            [SupportInboxController::class, 'countersMap']
-        )->name('admin.conversations.counters_map');
-
-        Route::get(
-            '/conversations/{business}/user/{user}/stream',
-            [SupportInboxController::class, 'stream']
-        )->name('admin.conversations.stream');
-    });
